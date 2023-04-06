f16d19df9949b202423387aac73909311a295fe9
unknown <ak4760r@gmail.com>
2023-04-06 15:35:04 +0800

    front&back connected

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import users from "./api/v1/users";
+
 const rootRouter = express.Router();
+
 rootRouter.use("/api/v1/users", users);
+
 export default rootRouter;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import services from "./services";
 
 function App() {
   const [count, setCount] = useState(0)
+
+  services.user.getAll().then((data) => {
+    console.log(data);
+    });
 
   return (
     <div className="App">
